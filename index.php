54a1e869fbd0b7681a4aae4d2d57b6b4d75f77a3
1136261505@qq.com <1136261505@qq.com>
2016-03-07 22:31:12 +0800

    公司的项目

--- index.php
+++ index.php
@@ -1,26 +1,22 @@
 <?php
 // +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2009 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
+// 定义ThinkPHP框架路径
+define('THINK_PATH', './ThinkPHP');
 
-// 应用入口文件
-header('Content-Type:text/html; charset=utf-8');
-// 检测PHP环境
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+//定义项目名称和路径
+define('APP_NAME', '');
+define('APP_PATH', './');
 
-// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG', True);
+// 加载框架入口文件
+require(THINK_PATH."/ThinkPHP.php");
 
-// 定义应用目录
-define('APP_PATH','./Application/');
-
-// 引入ThinkPHP入口文件
-require './ThinkPHP/ThinkPHP.php';
-
-// 亲^_^ 后面不需要任何代码了 就是如此简单
+App::run();
+?>
