1192999f6dfebbaabab9ee5830330be868a6b7cd
liujinxing <1136261505@qq.com>
2016-07-15 16:10:05 +0800

    liujx[yii2 advebd]

--- yii2/tests/codeception/config/config.php
+++ yii2/tests/codeception/config/config.php
@@ -1,11 +1,20 @@
 <?php
 /**
- * Application configuration shared by all test types
+ * Application configuration shared by all applications and test types
  */
 return [
+    'language' => 'en-US',
+    'controllerMap' => [
+        'fixture' => [
+            'class' => 'yii\faker\FixtureController',
+            'fixtureDataPath' => '@tests/codeception/common/fixtures/data',
+            'templatePath' => '@tests/codeception/common/templates/fixtures',
+            'namespace' => 'tests\codeception\common\fixtures',
+        ],
+    ],
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=localhost;dbname=yii2_basic_tests',
+            'dsn' => 'mysql:host=localhost;dbname=yii2_advanced_tests',
         ],
         'mailer' => [
             'useFileTransport' => true,
